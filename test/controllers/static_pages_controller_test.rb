a056abf70013519dd8985ca6026f93fc2abda09b
arihito <arihito.m@gmail.com>
2018-08-13 07:30:22 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,37 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Third"
+  end
+
+  test "should get method to root path" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get method to home action" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title","Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test "should get method to help action" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title","Help | #{@base_title}"
   end
 
+  test "should get method to about action" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title","About | #{@base_title}"
+  end
+  
+  test "should get method to contact action" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title","Contact | #{@base_title}"
+  end
 end
